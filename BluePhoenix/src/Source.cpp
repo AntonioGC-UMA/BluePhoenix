0057d8bb8b0d4f57be8a38ace6611cf0101466d9
AntonioGC <AntonioGC@uma.es>
2020-05-09 18:53:24 +0200

    Primer comit. Triangulos naranjas en movimiento

--- BluePhoenix/src/Source.cpp
+++ BluePhoenix/src/Source.cpp
@@ -86,6 +86,7 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Dibujar aqui !!!!1*/
